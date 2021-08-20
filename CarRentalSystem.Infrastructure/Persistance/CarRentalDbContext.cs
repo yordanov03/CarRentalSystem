67326bf0334fcf08015f3024190c70afe64e77fb
Svetoslav Yordanov <svetoslav.yordanov.003@gmail.com>
2021-08-20 12:13:34 +0300

    added dbContext

---
+++ CarRentalSystem.Infrastructure/Persistance/CarRentalDbContext.cs
@@ -0,0 +1,30 @@
+﻿using CarRentalSystem.Domain.Models.CarAds;
+using CarRentalSystem.Domain.Models.Dealers;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRentalSystem.Infrastructure.Persistance
+{
+    internal class CarRentalDbContext : DbContext
+    {
+        public CarRentalDbContext(DbContextOptions<CarRentalDbContext> options) : base(options) { }
+
+        public DbSet<CarAd> CarAds { get; set; } = default!;
+        public DbSet<Category> Categories { get; set; } = default!;
+        public DbSet<Manufacturer> Manufacturers { get; set; } = default!;
+        public DbSet<Dealer> Dealers { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(builder);
+        }
+
+
+    }
+}
