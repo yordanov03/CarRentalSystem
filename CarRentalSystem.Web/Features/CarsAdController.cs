60557a47f587a5bc2313092445236ef2ebb44788
Svetoslav Yordanov <svetoslav.yordanov.003@gmail.com>
2021-08-20 11:57:45 +0300

    added presentation layer

---
+++ CarRentalSystem.Web/Features/CarsAdController.cs
@@ -0,0 +1,21 @@
+﻿using CarRentalSystem.Domain.Models.CarAds;
+using CarRentalSystem.Domain.Models.Dealers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRentalSystem.Web.Features
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CarsAdController : ControllerBase
+    {
+        private static readonly Dealer Dealer = new Dealer("Dealer", "+123456789");
+
+        [HttpGet]
+        public IEnumerable<CarAd> Get() => Dealer.CarAds.Where(c => c.IsAvailable);
+    }
+}
