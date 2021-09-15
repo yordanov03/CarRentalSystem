381cfb46593e7a64146bc701a37ebd4fba29debc
Svetoslav Yordanov <svetoslav.yordanov.003@gmail.com>
2021-09-15 10:39:17 +0300

    registered CurrentUserService

--- CarRentalSystem.Web/WebConfiguration.cs
+++ CarRentalSystem.Web/WebConfiguration.cs
@@ -1,4 +1,6 @@
 ﻿using CarRentalSystem.Application.Common;
+using CarRentalSystem.Application.Contracts;
+using CarRentalSystem.Web.Services;
 using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,6 +12,7 @@ namespace CarRentalSystem.Web
         public static IServiceCollection AddWebComponents(this IServiceCollection services)
         {
             services
+                .AddScoped<ICurrentUser, CurrentUserService>()
                 .AddControllers()
                 .AddFluentValidation(validation => validation
                                      .RegisterValidatorsFromAssemblyContaining<Result>())
