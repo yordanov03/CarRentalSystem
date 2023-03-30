bc6e0a5ea8f07b6f42a492bd77cb89eae5099761
Svetoslav Yordanov <svetoslav.yordanov.003@gmail.com>
2023-03-30 12:44:49 +0200

    added swagger again

--- Startup/Startup.cs
+++ Startup/Startup.cs
@@ -10,6 +10,7 @@ namespace CarRentalSystem.Startup
     using Microsoft.Extensions.Hosting;
     using Web;
     using Web.Middleware;
+    using Microsoft.OpenApi.Models;
 
     public class Startup
     {
@@ -17,18 +18,39 @@ namespace CarRentalSystem.Startup
 
         public IConfiguration Configuration { get; }
 
-        public void ConfigureServices(IServiceCollection services) 
-            => services
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
                 .AddDomain()
                 .AddApplication(this.Configuration)
                 .AddInfrastructure(this.Configuration)
                 .AddWebComponents();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Car Rental System",
+                    Version = "v1",
+                    Description = "DDD CQRS"
+                });
+            });
+        }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+
+                app.UseSwagger();
+
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "CarRentalSystem");
+
+                    c.RoutePrefix = string.Empty;
+                });
             }
 
             app
